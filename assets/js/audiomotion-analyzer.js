b3ee681c4d2682f51534f5ffa7f473513a29d83e
Doutar User <doutar@example.com>
2025-12-05 00:59:05 +0330

    localized all cdns

---
+++ assets/js/audiomotion-analyzer.js
@@ -0,0 +1,16 @@
+/*
+ * Skypack CDN - audiomotion-analyzer@4.5.2
+ *
+ * Learn more:
+ *   📙 Package Documentation: https://www.skypack.dev/view/audiomotion-analyzer
+ *   📘 Skypack Documentation: https://www.skypack.dev/docs
+ *
+ * Pinned URL: (Optimized for Production)
+ *   ▶️ Normal: https://cdn.skypack.dev/pin/audiomotion-analyzer@v4.5.2-hmgaZTbqWXanircCxUW1/mode=imports/optimized/audiomotion-analyzer.js
+ *   ⏩ Minified: https://cdn.skypack.dev/pin/audiomotion-analyzer@v4.5.2-hmgaZTbqWXanircCxUW1/mode=imports,min/optimized/audiomotion-analyzer.js
+ *
+ */
+
+// Browser-Optimized Imports (Don't directly import the URLs below in your application!)
+export * from '/-/audiomotion-analyzer@v4.5.2-hmgaZTbqWXanircCxUW1/dist=es2019,mode=imports/optimized/audiomotion-analyzer.js';
+export {default} from '/-/audiomotion-analyzer@v4.5.2-hmgaZTbqWXanircCxUW1/dist=es2019,mode=imports/optimized/audiomotion-analyzer.js';
---
+++ assets/js/motion.js
@@ -0,0 +1,8 @@
+/**
+ * Bundled by jsDelivr using Rollup v2.79.2 and Terser v5.39.0.
+ * Original file: /npm/motion@10.18.0/dist/main.es.js
+ *
+ * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
+ */
+import{animate as o,withControls as n}from"/npm/@motionone/dom@10.18.0/+esm";export*from"/npm/@motionone/dom@10.18.0/+esm";export*from"/npm/@motionone/types@10.17.1/+esm";import{isFunction as m}from"/npm/@motionone/utils@10.18.0/+esm";import{Animation as t}from"/npm/@motionone/animation@10.18.0/+esm";function e(o,m={}){return n([()=>{const n=new t(o,[0,1],m);return n.finished.catch((()=>{})),n}],m,m.duration)}function r(n,t,r){return(m(n)?e:o)(n,t,r)}export{r as animate};export default null;
+//# sourceMappingURL=/sm/a8f20a8322fc748c51fe94ca54949c856f003c6f3f93af6c9a1fef2e9c14c5f8.map
